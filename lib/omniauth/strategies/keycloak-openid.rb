fe74885e4e23b6e0b39aa5a49a1da6d7eb35b789
Dinesh Budhayer <budhayer96d@gmail.com>
2021-01-15 13:46:03 +0545

    added homepage

--- lib/omniauth/strategies/keycloak-openid.rb
+++ lib/omniauth/strategies/keycloak-openid.rb
@@ -7,29 +7,25 @@ module OmniAuth
       DEFAULT_RESPONSE_TYPE = "code"
 
       option :name, 'keycloak_openid'
-      option :client_options, {
-          :site          => 'http://localhost:8080',
-          :authorize_url => 'http://localhost:8080/auth/realms/master/protocol/openid-connect/auth',
-          :token_url     => '/auth/realms/master/protocol/openid-connect/token',
-      }
+      option :client_options, site: 'http://localhost:8080'
 
-      option :authorize_options, [:scope, :response_type, :redirect_uri]
+      option :authorize_options, %i[scope response_type redirect_uri]
       option :provider_ignores_state, true
 
-      uid { @parsed_uid ||= (/([^\/]+)\z/.match raw_info['sub'])[1] }
+      uid { raw_info['user'] }
 
       info do
-        prune!({
-                   name: raw_info['given_name'] + raw_info['family_name'],
-                   email: raw_info['email'],
-                   first_name: raw_info['given_name'],
-                   last_name: raw_info['family_name'],
-                   phone: raw_info['phone_number']
-               })
+        {
+          name: raw_info['name'] || "#{raw_info['given_name']} #{raw_info['family_name']}",
+          email: raw_info['email'],
+          first_name: raw_info['given_name'],
+          last_name: raw_info['family_name'],
+          phone: raw_info['phone_number']
+        }
       end
 
       extra do
-        prune!({'raw_info' => raw_info})
+        prune!('raw_info' => raw_info)
       end
 
       def callback_url
@@ -47,23 +43,16 @@ module OmniAuth
         end
       end
 
-      def custom_build_access_token
-        access_token =
-            if request.xhr? && request.params['code']
-              verifier = request.params['code']
-              redirect_uri = request.params['redirect_uri'] || 'postmessage'
-              client.auth_code.get_token(verifier, get_token_options(redirect_uri), deep_symbolize(options.auth_token_params || {}))
-            elsif request.params['code'] && request.params['redirect_uri']
-              verifier = request.params['code']
-              redirect_uri = request.params['redirect_uri']
-              client.auth_code.get_token(verifier, get_token_options(redirect_uri), deep_symbolize(options.auth_token_params || {}))
-            else
-              verifier = request.params["code"]
-              client.auth_code.get_token(verifier, get_token_options(callback_url), deep_symbolize(options.auth_token_params))
-            end
-        access_token
+      def build_access_token
+        if options[:grant_type] == 'auth_code'
+          verifier = request.params['code']
+          client.auth_code.get_token(verifier, get_token_options(callback_url), deep_symbolize(options.auth_token_params))
+        else
+          client.password.get_token(request.params[:username],
+                                    request.params[:password],
+                                    scope: DEFAULT_SCOPE)
+        end
       end
-      alias_method :build_access_token, :custom_build_access_token
 
       private
 
@@ -71,11 +60,11 @@ module OmniAuth
         access_token.options[:mode] = :header
         access_token.options[:param_name] = :access_token
         access_token.options[:grant_type] = :authorization_code
-        access_token.get('/auth/realms/master/protocol/openid-connect/userinfo').parsed || {}
+        access_token.get(options[:userinfo_url]).parsed || {}
       end
 
       def get_token_options(redirect_uri)
-        { :redirect_uri => redirect_uri }.merge(token_params.to_hash(:symbolize_keys => true))
+        { redirect_uri: redirect_uri }.merge(token_params.to_hash(symbolize_keys: true))
       end
 
       def prune!(hash)
